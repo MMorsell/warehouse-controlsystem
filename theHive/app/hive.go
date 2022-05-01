0b4896dacd110f6e88f0ad65e51e77a96f6747e4
Martin Morsell <martinmorsell18@gmail.com>
2022-05-01 15:31:05 +0200

    Added bot service with client to server streaming

--- theHive/app/hive.go
+++ theHive/app/hive.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	botClientService "gits-15.sys.kth.se/Gophers/walle/theHive/api"
+	serviceContract "gits-15.sys.kth.se/Gophers/walle/theHive/proto"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	fmt.Println("Hello World!")
+	setupGRPCService()
+}
+
+func setupGRPCService() {
+	log.Printf("Starting GRPC service!")
+	portNumber := ":9738"
+	lis, err := net.Listen("tcp", portNumber)
+	if err != nil {
+		log.Fatalf("Failed to listen to %s to listen: %v", portNumber, err)
+	}
+
+	//Start custom service contract
+	s := botClientService.Server{}
+	grpcServer := grpc.NewServer()
+	serviceContract.RegisterBotClientServiceServer(grpcServer, &s)
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %s", err)
+	}
 }
