74604c2d61b1a84c2ce168870e5f48ce63dcb2e4
dante <dantew@kth.se>
2022-05-01 21:15:58 +0200

    Made Robot struct with move method, need more understanding of grpc for further development

--- walle/app/bot.go
+++ walle/app/bot.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+Recieve a list of instructions
+Report to hive as unavaliable
+Move according to these instructions *
+before moving, check with hive if it is OK to move
+after moving, respond back to hive with the new location
+Completing instructions, report back to hive as avaliable
+*/
+
+type Robot struct {
+	isAvailable  bool
+	isOkayToMove bool
+	PositionX    int
+	PositionY    int
+}
+
+func (robot *Robot) move(x int, y int) {
+	errorMessage := "can not move that far in one method call"
+	if robot.isOkayToMove && (!((x < 1 || x > 1) || (y < 1 || y > 1))) {
+		robot.PositionX += x
+		robot.PositionY += y
+	} else {
+		fmt.Println(errorMessage)
+	}
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	robot := &Robot{true, false, 0, 0}
+	fmt.Println(robot)
 }
