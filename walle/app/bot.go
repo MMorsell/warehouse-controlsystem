9baf4307596bd829ade61c3b6f39bff4ae23c3bd
dante <dantew@kth.se>
2022-05-02 15:35:12 +0200

    Updated Move methods, still no gRPC implementation

--- walle/app/bot.go
+++ walle/app/bot.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 /*
 Recieve a list of instructions
-Report to hive as unavaliable
 Move according to these instructions *
 before moving, check with hive if it is OK to move
 after moving, respond back to hive with the new location
-Completing instructions, report back to hive as avaliable
+Completing instructions, report back to hive as available
 */
 
 type Robot struct {
@@ -20,17 +20,38 @@ type Robot struct {
 	PositionY    int
 }
 
-func (robot *Robot) move(x int, y int) {
+type Coordinate struct {
+	xCoordinate int
+	yCoordinate int
+}
+
+func NewCoordinate(CoordinateX int, CoordinateY int) *Coordinate {
+	Coordinate := new(Coordinate)
+	Coordinate.xCoordinate = CoordinateX
+	Coordinate.yCoordinate = CoordinateY
+	return Coordinate
+}
+
+func (robot *Robot) move(coordinate Coordinate) {
 	errorMessage := "can not move that far in one method call"
-	if robot.isOkayToMove && (!((x < 1 || x > 1) || (y < 1 || y > 1))) {
-		robot.PositionX += x
-		robot.PositionY += y
+	if robot.isOkayToMove && (!((coordinate.xCoordinate < 1 || coordinate.xCoordinate > 1) || (coordinate.yCoordinate < 1 || coordinate.yCoordinate > 1))) {
+		robot.PositionX += coordinate.xCoordinate
+		robot.PositionY += coordinate.yCoordinate
 	} else {
 		fmt.Println(errorMessage)
 	}
 }
 
+func (robot *Robot) moveMultiple(listOfCoordinates ...Coordinate) {
+	for i := 0; i < len(listOfCoordinates); i++ {
+		robot.move(listOfCoordinates[i])
+	}
+	robot.isAvailable = true
+}
+
 func main() {
-	robot := &Robot{true, false, 0, 0}
+	maxX := 100 //To be decided, measurement for width of storage
+	maxY := 100 //To be decided, measurement for height of storage
+	robot := &Robot{true, false, rand.Intn(maxX), rand.Intn(maxY)}
 	fmt.Println(robot)
 }
