1fd6498c016e578174667fa07a8b58967efe3357
dante <dantew@kth.se>
2022-05-02 22:16:37 +0200

    Began working with gRPC

---
+++ walle/RobotService/RobotService.go
@@ -0,0 +1,10 @@
+package RobotService
+
+type Server struct {
+}
+
+//Implement askForTask function
+
+//func (s *Server) askForTask(ctx context.Context, message *IsAvailable) (*Instructions, error) {
+//log.Printf("Recieved ready from bot: %s", message.String())
+//}
---
+++ walle/Server/server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, error1 := net.Listen("tcp", "10")
+	if error1 != nil {
+		log.Fatalf("Failed to listen to port 10: %v", error1)
+	}
+	grpcServer := new(grpc.Server)
+
+	error2 := grpcServer.Serve(listener)
+
+	if error2 != nil {
+		log.Fatalf("Failed to provide grpc server over port 10: %v", error2)
+	}
+
+}
