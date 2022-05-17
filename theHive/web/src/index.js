8f6d8db0bceb5f1372cbd8b6a99b82b5dc8cad06
Martin Morsell <martinmorsell18@gmail.com>
2022-05-17 14:47:27 +0200

    bihang commit

--- theHive/web/src/index.js
+++ theHive/web/src/index.js
@@ -26,7 +26,7 @@ export function advancePath(instruction) {
     refCount[index]++;
 }
 
-function clearPath(id) {
+export function clearPath(id) {
     const agent = agents.get(id);
     clearAgentPath(agent, refCount);
 }
@@ -36,55 +36,3 @@ function XYtoLinear(x, y) {
 }
 
 makeGrid(rows, cols);
-
-/*
-advancePath({id: "1", x: 5, y: 2});
-advancePath({id: "1", x: 5, y: 3});
-advancePath({id: "1", x: 5, y: 4});
-advancePath({id: "1", x: 5, y: 5});
-advancePath({id: "1", x: 5, y: 5});
-advancePath({id: "2", x: 2, y: 3});
-advancePath({id: "2", x: 3, y: 3});
-advancePath({id: "2", x: 4, y: 3});
-advancePath({id: "2", x: 5, y: 3});
-advancePath({id: "2", x: 6, y: 3});
-advancePath({id: "2", x: 7, y: 3});
-//clearPath("2")
-*/
-
-async function mock() {
-    const paths = [
-        {id: "1", x: 5, y: 7},
-        {id: "2", x: 9, y: 5},
-        {id: "1", x: 6, y: 7},
-        {id: "2", x: 9, y: 6},
-        {id: "1", x: 7, y: 7},
-        {id: "2", x: 9, y: 7},
-        {id: "1", x: 8, y: 7},
-        {id: "2", x: 9, y: 8},
-        {id: "1", x: 9, y: 7},
-        {id: "2", x: 9, y: 9},
-        {id: "1", x: 10, y: 7},
-        {id: "2", x: 9, y: 10},
-        {id: "1", x: 11, y: 7},
-        {id: "2", x: 9, y: 11},
-        {id: "1", x: 12, y: 7},
-        {id: "2", x: 9, y: 12},
-        {id: "1", x: 13, y: 7},
-        {id: "2", x: 9, y: 13},
-        {id: "1", x: 14, y: 7},
-        {id: "2", x: 9, y: 14},
-        {id: "1", x: 15, y: 7},
-        {id: "2", x: 9, y: 15},
-        {id: "1", x: 16, y: 7},
-    ];
-
-    for(let i = 0; i < paths.length - 1; i += 2) {
-        advancePath(paths[i]);
-        advancePath(paths[i+1]);
-        await new Promise(r => setTimeout(r, 500));
-    }
-}
-
-
-
